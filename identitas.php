15caf32c3490afa01faca2f2d8971d1a9a21b5f6
161710089 <33051548+161710089@users.noreply.github.com>
2017-10-24 17:38:41 +0700

    Update identitas.php

--- identitas.php
+++ identitas.php
@@ -3,7 +3,7 @@
 require_once 'identitas.php';
 $orang =new orang ('Alfi','Islam','XI RPL 1','Pelajar');
 echo "Namanya.. ".$orang->get_nama()."<br>";
-echo "Tempat Lahir.. ".$orang->get_agama()."<br>";
+echo "Agama.. ".$orang->get_agama()."<br>";
 echo "Kelas.. ".$orang->get_kelas()."<br>";
 echo "Status.. ".$orang->get_status()."<br>";
 
